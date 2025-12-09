ce7be8a779992aac9f4870a118c971e48d5cd793
izzaldev <rizal.fadlullah@gmail.com>
2025-12-09 22:58:51 +0700

    chore: setup layout

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.lightBlue,
+        ),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -27,38 +29,93 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _selectedIndex = 0;
 
-  void _incrementCounter() {
+  final List<({IconData icon, IconData selectedIcon, String label})> _navItems =
+      const [
+        (
+          icon: Icons.home_outlined,
+          selectedIcon: Icons.home,
+          label: "Home",
+        ),
+        (
+          icon: Icons.settings_outlined,
+          selectedIcon: Icons.settings,
+          label: "Settings",
+        ),
+        (
+          icon: Icons.history_outlined,
+          selectedIcon: Icons.history,
+          label: "History",
+        ),
+      ];
+
+  void _onItemTapped(int index) {
     setState(() {
-     _counter++;
+      _selectedIndex = index;
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    bool isLargeScreen = MediaQuery.of(context).size.width >= 600;
+    final theme = Theme.of(context);
+
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: .center,
+      body: SafeArea(
+        child: Row(
           children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            if (isLargeScreen)
+              NavigationRail(
+                minWidth: 50,
+                backgroundColor: theme.colorScheme.primaryContainer,
+                indicatorColor: Colors.transparent,
+                indicatorShape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                selectedIndex: _selectedIndex, // Gunakan state
+                onDestinationSelected:
+                    _onItemTapped, // Update state saat dipilih
+                destinations: _navItems.map((item) {
+                  return NavigationRailDestination(
+                    icon: Icon(
+                      item.icon,
+                      size: 24,
+                    ),
+                    selectedIcon: Icon(
+                      item.selectedIcon,
+                      size: 24,
+                    ),
+                    label: Text(item.label),
+                  );
+                }).toList(),
+              ),
+            Expanded(
+              child: Center(
+                child: Text("Hellow World - Selected Index: $_selectedIndex"),
+              ),
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () {},
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ),
+      bottomNavigationBar: isLargeScreen
+          ? null
+          : BottomNavigationBar(
+              currentIndex: _selectedIndex, // Gunakan state
+              onTap: _onItemTapped, // Update state saat dipilih
+              items: _navItems.map((item) {
+                return BottomNavigationBarItem(
+                  icon: Icon(item.icon),
+                  activeIcon: Icon(item.selectedIcon), // Ikon yang diisi/solid
+                  label: item.label,
+                );
+              }).toList(),
+            ),
     );
   }
 }
